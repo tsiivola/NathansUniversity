4e3363b821679421d57c4fa0b57678ef349a59e1
Tommi Siivola <tommi.siivola@gmail.com>
2012-04-17 20:06:48 +0300

    Basic compiler and test

---
+++ mus/compiler.js
@@ -0,0 +1,39 @@
+var endTime = function(t, e) {
+    if (e.tag === 'note')
+        return t+e.dur;
+    if (e.tag === 'seq')
+        return endTime(t, e.left) + endTime(t, e.right);
+    if (e.tag === 'par') {
+        return Math.max(t, 
+            endTime(t, e.left), endTime(t, e.right));
+    }
+};
+
+var compileT = function(t, e) {
+    if (e.tag === 'note')
+        return [ { tag: 'note', pitch: e.pitch, 
+                 start: t, dur: e.dur } ];
+    if (e.tag === 'seq') 
+        return compileT(t, e.left).concat( compileT(endTime(t, e.left), e.right) );
+    if (e.tag === 'par')
+        return compileT(t, e.left).concat( compileT(t, e.right) );
+};
+
+var compile = function (musexpr) {
+    return compileT(0, musexpr);
+};
+
+
+var melody_mus = 
+    { tag: 'seq',
+      left: 
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'a4', dur: 250 },
+         right: { tag: 'note', pitch: 'b4', dur: 250 } },
+      right:
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'c4', dur: 500 },
+         right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+
+console.log(melody_mus);
+console.log(compile(melody_mus));
