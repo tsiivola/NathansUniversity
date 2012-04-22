552617f22b6457dbde692a56a6d7df7485f21a84
Tommi Siivola <tommi.siivola@gmail.com>
2012-04-22 11:27:47 +0300

    Parser learned about multiple space characters
    Atoms in an expression can now be separated by more than one space

--- scheem/scheem.js
+++ scheem/scheem.js
@@ -11,7 +11,7 @@ fs.readFile('scheem.peg', 'ascii', function(err, data) {
     assert.deepEqual( parse("(a b c)"), ["a", "b", "c"] );
     assert.deepEqual( parse("(a (b c))"), ["a", ["b", "c"]] );
     assert.deepEqual( parse("(+ (* (+ b c) 5 6))"), ["+", ["*", ["+", "b", "c"], "5", "6"]] );
-    
+    assert.deepEqual( parse("(+   (*  (+  b c)  5 6))"), ["+", ["*", ["+", "b", "c"], "5", "6"]] );
 });
 
 
