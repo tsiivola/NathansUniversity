e7279ff1a1bdd07da7d7cfb0503da99ea5063590
Tommi Siivola <tommi.siivola@gmail.com>
2012-04-22 11:40:47 +0300

    Parser learned more lenient whitespace handling

--- scheem/scheem.js
+++ scheem/scheem.js
@@ -12,6 +12,12 @@ fs.readFile('scheem.peg', 'ascii', function(err, data) {
     assert.deepEqual( parse("(a (b c))"), ["a", ["b", "c"]] );
     assert.deepEqual( parse("(+ (* (+ b c) 5 6))"), ["+", ["*", ["+", "b", "c"], "5", "6"]] );
     assert.deepEqual( parse("(+   (*  (+  b c)  5 6))"), ["+", ["*", ["+", "b", "c"], "5", "6"]] );
+    assert.deepEqual( parse("(+   (  *  ( +  b c  ) 5 6))"), ["+", ["*", ["+", "b", "c"], "5", "6"]] );
+    assert.deepEqual( parse("\
+(+ (* \
+   (+ b c)\
+   5 6))"),
+                    ["+", ["*", ["+", "b", "c"], "5", "6"]] );
 });
 
 
