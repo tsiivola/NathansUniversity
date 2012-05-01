a5c5c5d9efcbdfde4778e5a299ec2ed36b09bdc3
Tommi Siivola <tommi.siivola@gmail.com>
2012-05-01 17:26:52 +0300

    Parser learned to handle numbers
    Numbers in string form are now converted to actual numbers in AST

--- scheem/scheem_parser.js
+++ scheem/scheem_parser.js
@@ -8,16 +8,18 @@ fs.readFile('scheem.peg', 'ascii', function(err, data) {
     // Create my parser
     parse = PEG.buildParser(data).parse;
     // Do a test
+    assert.strictEqual(parse("56"), 56);
+    assert.strictEqual(parse("0"), 0);
     assert.deepEqual( parse("(a b c)"), ["a", "b", "c"] );
     assert.deepEqual( parse("(a (b c))"), ["a", ["b", "c"]] );
-    assert.deepEqual( parse("(+ (* (+ b c) 5 6))"), ["+", ["*", ["+", "b", "c"], "5", "6"]] );
-    assert.deepEqual( parse("(+   (*  (+  b c)  5 6))"), ["+", ["*", ["+", "b", "c"], "5", "6"]] );
-    assert.deepEqual( parse("(+   (  *  ( +  b c  ) 5 6))"), ["+", ["*", ["+", "b", "c"], "5", "6"]] );
+    assert.deepEqual( parse("(+ (* (+ b c) 5 6))"), ["+", ["*", ["+", "b", "c"], 5, 6]] );
+    assert.deepEqual( parse("(+   (*  (+  b c)  5 6))"), ["+", ["*", ["+", "b", "c"], 5, 6]] );
+    assert.deepEqual( parse("(+   (  *  ( +  b c  ) 5 6))"), ["+", ["*", ["+", "b", "c"], 5, 6]] );
     assert.deepEqual( parse("\
 (+ (* \
    (+ b c)\
    5 6))"),
-                    ["+", ["*", ["+", "b", "c"], "5", "6"]] );
+                    ["+", ["*", ["+", "b", "c"], 5, 6]] );
     assert.deepEqual( parse("\
 ;; factorial\n\
 (define factorial (n) ;; takes one argument n \n\
@@ -26,7 +28,7 @@ fs.readFile('scheem.peg', 'ascii', function(err, data) {
        (factorial (- n 1))))\
 ;; factorial\n\
 "),
-                    ["define", "factorial", ["n"], ["*", "n", ["factorial", ["-", "n", "1"]]]]);
+                    ["define", "factorial", ["n"], ["*", "n", ["factorial", ["-", "n", 1]]]]);
     assert.deepEqual(parse("'asdf"), ["quote", "asdf"]);
     assert.deepEqual(parse("'(a (b c))"), ["quote", ["a", ["b", "c"]]]);
     assert.deepEqual(parse("(a '(b c))"), ["a", ["quote", ["b", "c"]]]);
