39ee558dd675b028d88fe96f4a7dc7328443be5c
Tommi Siivola <tommi.siivola@gmail.com>
2012-05-21 20:55:45 +0300

    Miscellaneous stuff to get lambda working

--- scheem/scheem.js
+++ scheem/scheem.js
@@ -38,7 +38,58 @@ var comparison = function(expr, env) {
     }
 }
 
+var lookup = function (env, v) {
+    if (env.name === v) return env.value;
+    else {
+        if (env.outer === undefined) return undefined;
+        return lookup(env.outer, v);
+    }
+};
+
+var env_to_s = function (env) {
+    if (env.name === undefined) return "";
+    if (env.outer === undefined) return env.name + ":" + env.value;
+    return env.name + ":" + env.value + "\n" + env_to_s(env.outer);
+};
+
+var expr_to_s = function (expr) {
+    if (expr instanceof Array) {
+        var s = "(";
+        for (i=0; i<expr.length; i+=1) {
+            s += expr_to_s(expr[i]) + " ";
+        }
+        return s + ")";
+    }
+    else return "" + expr;
+};
+
+var update = function (env, v, val) {
+    if (env.name === v) 
+        env.value = val;
+    else {
+        if (!env.outer) throw new Error("Attempted to change non-existing value " + v);
+        update(env.outer, v, val);
+    }
+    return 0;
+};
+
+var add_binding = function (env, v, val) {
+    return { outer: env, name: v, value: val };
+};
+
+var _add_binding = function (env, v, val) {
+    env.outer = { outer: env.outer, name: env.name, value: env.value };
+    env.name = v;
+    env.value = val;
+    return env;
+};
+
+global_debug = undefined;
+
 var evalScheem = function (expr, env) {
+    if (global_debug == true) {
+        global_debug = confirm("Evaluating:\n" + expr_to_s(expr) + "\n\nEnvironment:\n" + env_to_s(env));
+    }
     // Numbers evaluate to themselves
     if (typeof expr === 'number') {
         return expr;
@@ -47,9 +98,9 @@ var evalScheem = function (expr, env) {
     if (typeof expr === 'string') {
         // support for boolean values
         if (expr === '#t' || expr === '#f') return expr;
-        var v = env[expr];
-        if (v === undefined) throw new Error(expr + " not defined!");
-        return env[expr];
+        var v = lookup(env, expr);
+        if (v === undefined) throw new Error(expr + " undefined!");
+        return v;
     }
     // Look at head of list for operation
     switch (expr[0]) {
@@ -66,16 +117,16 @@ var evalScheem = function (expr, env) {
             return expr[1];
         case 'begin':
             var i, n = (expr.length - 1);
-            for(i=1;i<n; i++) {
+            for(i=1;i<n; i+=1) {
                 evalScheem(expr[i], env);
             }
             return evalScheem(expr[i], env);
         case 'define':
-            if (env[expr[1]]) throw new Error("Attempted re-definition of: " + expr[1]);
-            env[expr[1]] = evalScheem(expr[2], env); return 0;
+            if (lookup(env, expr[1])) throw new Error("Attempted re-definition of: " + expr[1]);
+            _add_binding(env, expr[1], evalScheem(expr[2], env));
         case 'set!':
-            if (env[expr[1]] === undefined) throw new Error("Attempted set! of non-existing variable: " + expr[1]);
-            env[expr[1]] = evalScheem(expr[2], env); return 0;
+            if (!lookup(env, expr[1])) throw new Error("Attempted set! of non-existing variable: " + expr[1]);
+            update(env, expr[1], evalScheem(expr[2], env));
         case 'cons':
             return [evalScheem(expr[1], env)].concat(evalScheem(expr[2], env));
         case 'car':
@@ -85,6 +136,23 @@ var evalScheem = function (expr, env) {
         case 'if':
             if (evalScheem(expr[1], env) === '#t') return evalScheem(expr[2], env);
             else return evalScheem(expr[3], env);
+        case 'lambda':
+            return function() {
+                var l = expr.length;
+                var e = env;
+                for (i=1; i<(l-1); i+=1) {
+                    e = add_binding(e, expr[i], evalScheem(arguments[i-1], env));
+                }
+                return evalScheem(expr[i], e);
+            };
+        default:
+            var f = evalScheem(expr[0], env);
+            var args = [];
+            var l = expr.length;
+            for (i=1; i<l; i+=1) {
+                args.push(evalScheem(expr[i], env));
+            }
+            return f.apply(this, args);
     }
 };
 
--- scheem/test/evalScheemStringTest.js
+++ scheem/test/evalScheemStringTest.js
@@ -17,5 +17,20 @@ suite('eval', function() {
     test('a begin expression', function() {
         assert.deepEqual(evalScheemString("(begin (define a 5) (define b 6) (+ a b))"), 11);
     });
+    test('a lambda expression', function() {
+        assert.deepEqual(evalScheemString("( (lambda a b c (+ (* a b) c)) 3 4 5 6  7)"), 17);
+    });
+    test('a stored lambda expression', function() {
+        assert.deepEqual(evalScheemString(
+"(begin (define min-of-two (lambda a b (if (< a b) a b)))\
+ (min-of-two 6 36))"), 6);
+    });
+    test('a recursive function definition', function() {
+        assert.deepEqual(evalScheemString(
+"(begin \
+  (define factorial \
+    (lambda n (if (= n 0) 1 \
+                          (* n (factorial (- n 1)))))) \
+(factorial 12))"), 479001600);
 });
-
+});
